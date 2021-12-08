62ec27b03069d4436ebde3e049dc34327da369cd
CharlesDeda <94919280+CharlesDeda@users.noreply.github.com>
2021-12-08 13:14:23 -0500

    Initial Commit

---
+++ DedaSuns/DedaSunsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DedaSunsApp.swift
+//  DedaSuns
+//
+//  Created by Nick Deda on 12/8/21.
+//
+
+import SwiftUI
+
+@main
+struct DedaSunsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
