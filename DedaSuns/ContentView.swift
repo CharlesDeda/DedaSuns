0014953cfc4d26accd52545de92ad4335bb1dd4c
CharlesDeda <94919280+CharlesDeda@users.noreply.github.com>
2021-12-08 13:53:37 -0500

    create basic view

--- DedaSuns/ContentView.swift
+++ DedaSuns/ContentView.swift
@@ -8,14 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("DedaSuns")
-            .padding()
+  @State var max = 235
+  var body: some View {
+    NavigationView {
+      Form {
+        Stepper("Max \(max)", value: $max)
+        
+        Section {
+          List(NsunsWorkout(max: max).nsunsSets) { nsuns in
+            HStack {
+              Text("Percent: \(nsuns.percentage)%")
+              Text("Weight: \(nsuns.weight)")
+              Text("Reps: \(nsuns.reps)")
+            }
+          }
+        }
+      }
+      .navigationTitle("Deda Suns")
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
